e125414af8dbc1b713a33a7b819ac6c123ed9f3c
Luke Ramsay <lukram98@gmail.com>
2018-06-27 12:49:22 -0400

    Updates to functionality & ui

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -6,7 +6,7 @@ var giphs = ["car", "soccer", "basketball"];
 
 function displayInfo() {
 
-  var searchTerm = $("#giphy-input").val();
+  var searchTerm = $("#giphy-input").val().trim();
   var giphName = $(this).attr("data-name");
   var apiKey = "6Qny4dlr1HaDqscS3xo7ydJJ2DNAsDQ1";
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphName + "&api_key="+ apiKey +"&limit=5";
@@ -15,12 +15,39 @@ function displayInfo() {
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response.data[0].images.original.url);
+        for (var i = 0; i < response.data.length; i++) {
         var $giphyImg = $("<img>")
-        $giphyImg.attr("src", response.data[0].images.original.url);
+        $giphyImg.attr("data-still", response.data[i].images.original_still.url)
+          .attr("data-animate", response.data[i].images.original.url)
+          .attr("state", "still")
+          .attr("src", $giphyImg.attr("data-still"))
+          .attr("width", "225")
+          .attr("height", "125")
+          .attr("data-giphname", giphs[i])
+          .addClass("giph-img")
+          .css("padding", "10px");
         $("#giphy-view").append($giphyImg);
+      }
   });
 
+
+}
+
+function updateState() {
+$(".giph-img").on("click", function() {
+  var state = $(this).attr("state");
+
+for (var i = 0; i < giphs.length; i++) {
+  if (state == "still" && $("")) {
+    $(this).attr("src", $(this).attr("data-animate"))
+    .attr("state", "animate");
+  }
+  else if (state =="animate") {
+    $(this).attr("src", $(this).attr("data-still"))
+    .attr("state", "still");
+  }
+}
+});
 }
 
 
@@ -30,9 +57,12 @@ function renderButtons() {
 
     for (var i = 0; i < giphs.length; i++) {
       var giphButton = $("<button>");
-      giphButton.addClass("giph");
-      giphButton.attr("data-name", giphs[i]);
-      giphButton.text(giphs[i]);
+      giphButton.addClass("giph")
+        .attr("data-name", giphs[i])
+        .text(giphs[i])
+        .addClass("btn")
+        .addClass("btn-info")
+        .css("margin", "5px");
       $("#buttons-view").append(giphButton);
     }
 }
@@ -52,3 +82,9 @@ $("#add-giphy").on("click", function(event) {
   });
 
   $(document).on("click", ".giph", displayInfo);
+  $(document).on("click", ".giph-img", updateState);
+
+
+  $("#giphy-submit").on("click", function(event) {
+    event.preventDefault();
+  });
